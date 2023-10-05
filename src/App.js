57470aacd8d05828ea636ea706440fe985614974
John Maybee <john.d.maybee@gmail.com>
2023-10-05 17:29:55 -0400

    created some react code within the App.js to make sure react was setup properly and was rendering on the dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
 
 const App = () => {
+  const name = 'John';
+
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
+      <h1>Hello, {name}!</h1>
+      {name ? (
+        <>
+          <h1>{name}</h1>
+        </>
+      ): (
+        <>
+          <h1>test</h1>
+          <h2>There is no name</h2>
+        </>
+      )}
     </div>
   );
 }
 
 export default App;
+
+// Previously used code snippets: //
+
+// {isNameShowing ? name : 'No name'}
+
+
+//  const App = () => {
+//  const name = 'John';
+
+//   return (
+//     <div className="App">
+//       <h1>Hello, {name}!</h1>
+//       {name ? (
+//         <>
+//           <h1>{name}</h1>
+//         </>
+//       ): (
+//         <>
+//           <h1>test</h1>
+//           <h2>There is no name</h2>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
