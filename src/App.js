fd31461cbd9ecdedaffdc4d8471b99c8bcb6bb8d
John Maybee <john.d.maybee@gmail.com>
2023-10-09 18:45:14 -0400

    setup app.js so it would take in one of the movies from the api to make sure the API was working

--- src/App.js
+++ src/App.js
@@ -1,46 +1,67 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from "react";
+
 import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com?apikey=3b40fec7';
+
+const movie1 = {
+    "Title": "Batman",
+    "Year": "1989",
+    "imdbID": "tt0096895",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg"
+};
 
 const App = () => {
-  const [count, setCount] = useState(0);
 
-  // Initial render of the count due to useEffect hook is 5 //
-  useEffect(() => {
-    setCount(5);
-  }, []);
+   
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
 
-  return (
-    <div className="App">
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
-    </div>
-  );
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('batman');
+    }, []);
+   
+    return (
+        <div className="app">
+            <h1>Film Search</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value="Superman"
+                    onChange={() => {}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
-
-// Previously used code snippets: //
-
-// {isNameShowing ? name : 'No name'}
-
-
-//  const App = () => {
-//  const name = 'John';
-
-//   return (
-//     <div className="App">
-//       <h1>Hello, {name}!</h1>
-//       {name ? (
-//         <>
-//           <h1>{name}</h1>
-//         </>
-//       ): (
-//         <>
-//           <h1>test</h1>
-//           <h2>There is no name</h2>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
